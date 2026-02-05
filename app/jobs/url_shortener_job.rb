a3e4f14b2d6239ec738b6e700fc54b6eac8dd0a8
Renan <renanthompsom@gmail.com>
2026-02-05 11:08:32 -0300

    fix: github url shortio url

--- app/jobs/url_shortener_job.rb
+++ app/jobs/url_shortener_job.rb
@@ -3,12 +3,14 @@ class UrlShortenerJob < ApplicationJob
 
   sidekiq_options retry: false
 
+  GITHUB_URL = "https://github.com"
+
   def perform(profile_id)
     profile = Profile.find(profile_id)
 
     Rails.logger.info "=== UrlShortenerJob iniciado para profile_id: #{profile_id} ==="
 
-    github_url = "https://www.github.com.br/#{profile.github_username}"
+    github_url = "#{GITHUB_URL}/#{profile.github_username}"
     service = ShortioUrlShortenerService.new(github_url)
     short_github_url = service.call[:short_url]
 
