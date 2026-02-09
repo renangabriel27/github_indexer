6208fb683aa3c0f6a1cb3628781b75767311ada7
Renan <renanthompsom@gmail.com>
2026-02-09 17:39:21 -0300

    feat: update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,504 @@ Profile.destroy_all
 
 Profile.create!([
   {
+    name: "Misko Hevery",
+    short_github_url: "https://github.com/mhevery",
+    github_username: "mhevery",
+    avatar_url: "https://avatars.githubusercontent.com/u/111127?v=4",
+    followers: 12000,
+    following: 100,
+    location: "California",
+    scraping_status: "completed"
+  },
+  {
+    name: "Simon Willison",
+    short_github_url: "https://github.com/simonw",
+    github_username: "simonw",
+    avatar_url: "https://avatars.githubusercontent.com/u/9599?v=4",
+    followers: 28000,
+    following: 500,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "Ives van Hoorne",
+    short_github_url: "https://github.com/CompuIves",
+    github_username: "CompuIves",
+    avatar_url: "https://avatars.githubusercontent.com/u/587016?v=4",
+    followers: 8000,
+    following: 80,
+    location: "Netherlands",
+    scraping_status: "completed"
+  },
+  {
+    name: "Robin Wieruch",
+    short_github_url: "https://github.com/rwieruch",
+    github_username: "rwieruch",
+    avatar_url: "https://avatars.githubusercontent.com/u/2479967?v=4",
+    followers: 12000,
+    following: 100,
+    location: "Berlin, Germany",
+    scraping_status: "completed"
+  },
+  {
+    name: "Andrea Giammarchi",
+    short_github_url: "https://github.com/WebReflection",
+    github_username: "WebReflection",
+    avatar_url: "https://avatars.githubusercontent.com/u/85749?v=4",
+    followers: 5000,
+    following: 50,
+    location: "Germany",
+    scraping_status: "completed"
+  },
+  {
+    name: "Evan Czaplicki",
+    short_github_url: "https://github.com/evancz",
+    github_username: "evancz",
+    avatar_url: "https://avatars.githubusercontent.com/u/1658058?v=4",
+    followers: 10000,
+    following: 0,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "Tobias Koppers",
+    short_github_url: "https://github.com/sokra",
+    github_username: "sokra",
+    avatar_url: "https://avatars.githubusercontent.com/u/1365881?v=4",
+    followers: 12000,
+    following: 50,
+    location: "Germany",
+    scraping_status: "completed"
+  },
+  {
+    name: "Jason Miller",
+    short_github_url: "https://github.com/developit",
+    github_username: "developit",
+    avatar_url: "https://avatars.githubusercontent.com/u/105127?v=4",
+    followers: 22000,
+    following: 100,
+    location: "Toronto",
+    scraping_status: "completed"
+  },
+  {
+    name: "Monica Dinculescu",
+    short_github_url: "https://github.com/notwaldorf",
+    github_username: "notwaldorf",
+    avatar_url: "https://avatars.githubusercontent.com/u/1369170?v=4",
+    followers: 8000,
+    following: 150,
+    location: "Brooklyn, NY",
+    scraping_status: "completed"
+  },
+  {
+    name: "Jake Archibald",
+    short_github_url: "https://github.com/jakearchibald",
+    github_username: "jakearchibald",
+    avatar_url: "https://avatars.githubusercontent.com/u/93594?v=4",
+    followers: 16000,
+    following: 100,
+    location: "London, UK",
+    scraping_status: "completed"
+  },
+  {
+    name: "Nicolas Gallagher",
+    short_github_url: "https://github.com/necolas",
+    github_username: "necolas",
+    avatar_url: "https://avatars.githubusercontent.com/u/239676?v=4",
+    followers: 14000,
+    following: 50,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "Chris Coyier",
+    short_github_url: "https://github.com/chriscoyier",
+    github_username: "chriscoyier",
+    avatar_url: "https://avatars.githubusercontent.com/u/32376?v=4",
+    followers: 18000,
+    following: 200,
+    location: "Bend, OR",
+    scraping_status: "completed"
+  },
+  {
+    name: "Lea Verou",
+    short_github_url: "https://github.com/LeaVerou",
+    github_username: "LeaVerou",
+    avatar_url: "https://avatars.githubusercontent.com/u/175836?v=4",
+    followers: 15000,
+    following: 100,
+    location: "Cambridge, MA",
+    scraping_status: "completed"
+  },
+  {
+    name: "Cassidy Williams",
+    short_github_url: "https://github.com/cassidoo",
+    github_username: "cassidoo",
+    avatar_url: "https://avatars.githubusercontent.com/u/1454517?v=4",
+    followers: 24000,
+    following: 300,
+    location: "Chicago",
+    scraping_status: "completed"
+  },
+  {
+    name: "Una Kravets",
+    short_github_url: "https://github.com/una",
+    github_username: "una",
+    avatar_url: "https://avatars.githubusercontent.com/u/1693164?v=4",
+    followers: 12000,
+    following: 150,
+    location: "Brooklyn, NY",
+    scraping_status: "completed"
+  },
+  {
+    name: "Sarah Drasner",
+    short_github_url: "https://github.com/sdras",
+    github_username: "sdras",
+    avatar_url: "https://avatars.githubusercontent.com/u/2281088?v=4",
+    followers: 28000,
+    following: 200,
+    location: "Seattle",
+    scraping_status: "completed"
+  },
+  {
+    name: "Andrew Clark",
+    short_github_url: "https://github.com/acdlite",
+    github_username: "acdlite",
+    avatar_url: "https://avatars.githubusercontent.com/u/3624098?v=4",
+    followers: 18000,
+    following: 100,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "Ben Awad",
+    short_github_url: "https://github.com/benawad",
+    github_username: "benawad",
+    avatar_url: "https://avatars.githubusercontent.com/u/7872329?v=4",
+    followers: 30000,
+    following: 20,
+    location: "Austin, TX",
+    scraping_status: "completed"
+  },
+  {
+    name: "Max Howell",
+    short_github_url: "https://github.com/mxcl",
+    github_username: "mxcl",
+    avatar_url: "https://avatars.githubusercontent.com/u/58962?v=4",
+    followers: 22000,
+    following: 50,
+    location: "London",
+    scraping_status: "completed"
+  },
+  {
+    name: "Wes Bos",
+    short_github_url: "https://github.com/wesbos",
+    github_username: "wesbos",
+    avatar_url: "https://avatars.githubusercontent.com/u/176013?v=4",
+    followers: 35000,
+    following: 100,
+    location: "Hamilton, Ontario",
+    scraping_status: "completed"
+  },
+  {
+    name: "Brad Traversy",
+    short_github_url: "https://github.com/bradtraversy",
+    github_username: "bradtraversy",
+    avatar_url: "https://avatars.githubusercontent.com/u/5550850?v=4",
+    followers: 48000,
+    following: 10,
+    location: "Massachusetts",
+    scraping_status: "completed"
+  },
+  {
+    name: "Theo Browne",
+    short_github_url: "https://github.com/t3dotgg",
+    github_username: "t3dotgg",
+    avatar_url: "https://avatars.githubusercontent.com/u/6751787?v=4",
+    followers: 25000,
+    following: 100,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "Rich Harris",
+    short_github_url: "https://github.com/Rich-Harris",
+    github_username: "Rich-Harris",
+    avatar_url: "https://avatars.githubusercontent.com/u/1162160?v=4",
+    followers: 28000,
+    following: 50,
+    location: "New York",
+    scraping_status: "completed"
+  },
+  {
+    name: "Guillermo Rauch",
+    short_github_url: "https://github.com/rauchg",
+    github_username: "rauchg",
+    avatar_url: "https://avatars.githubusercontent.com/u/13041?v=4",
+    followers: 55000,
+    following: 400,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "John Resig",
+    short_github_url: "https://github.com/jeresig",
+    github_username: "jeresig",
+    avatar_url: "https://avatars.githubusercontent.com/u/70?v=4",
+    followers: 32000,
+    following: 200,
+    location: "Brooklyn, NY",
+    scraping_status: "completed"
+  },
+  {
+    name: "Paul Irish",
+    short_github_url: "https://github.com/paulirish",
+    github_username: "paulirish",
+    avatar_url: "https://avatars.githubusercontent.com/u/39191?v=4",
+    followers: 38000,
+    following: 250,
+    location: "California",
+    scraping_status: "completed"
+  },
+  {
+    name: "Addy Osmani",
+    short_github_url: "https://github.com/addyosmani",
+    github_username: "addyosmani",
+    avatar_url: "https://avatars.githubusercontent.com/u/110953?v=4",
+    followers: 42000,
+    following: 300,
+    location: "Mountain View, CA",
+    scraping_status: "completed"
+  },
+  {
+    name: "TJ Holowaychuk",
+    short_github_url: "https://github.com/tj",
+    github_username: "tj",
+    avatar_url: "https://avatars.githubusercontent.com/u/25254?v=4",
+    followers: 45000,
+    following: 100,
+    location: "Canada",
+    scraping_status: "completed"
+  },
+  {
+    name: "Sindre Sorhus",
+    short_github_url: "https://github.com/sindresorhus",
+    github_username: "sindresorhus",
+    avatar_url: "https://avatars.githubusercontent.com/u/170270?v=4",
+    followers: 75000,
+    following: 0,
+    location: "Thailand",
+    scraping_status: "completed"
+  },
+  {
+    name: "Jordan Walke",
+    short_github_url: "https://github.com/jordwalke",
+    github_username: "jordwalke",
+    avatar_url: "https://avatars.githubusercontent.com/u/606176?v=4",
+    followers: 8000,
+    following: 80,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "Sebastian McKenzie",
+    short_github_url: "https://github.com/kittens",
+    github_username: "kittens",
+    avatar_url: "https://avatars.githubusercontent.com/u/853712?v=4",
+    followers: 12000,
+    following: 50,
+    location: "Australia",
+    scraping_status: "completed"
+  },
+  {
+    name: "Ryan Florence",
+    short_github_url: "https://github.com/ryanflorence",
+    github_username: "ryanflorence",
+    avatar_url: "https://avatars.githubusercontent.com/u/100200?v=4",
+    followers: 35000,
+    following: 150,
+    location: "Utah",
+    scraping_status: "completed"
+  },
+  {
+    name: "Sophie Alpert",
+    short_github_url: "https://github.com/sophiebits",
+    github_username: "sophiebits",
+    avatar_url: "https://avatars.githubusercontent.com/u/6820?v=4",
+    followers: 15000,
+    following: 100,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "Kent C. Dodds",
+    short_github_url: "https://github.com/kentcdodds",
+    github_username: "kentcdodds",
+    avatar_url: "https://avatars.githubusercontent.com/u/1500684?v=4",
+    followers: 50000,
+    following: 200,
+    location: "Utah",
+    scraping_status: "completed"
+  },
+  {
+    name: "Taylor Otwell",
+    short_github_url: "https://github.com/taylorotwell",
+    github_username: "taylorotwell",
+    avatar_url: "https://avatars.githubusercontent.com/u/463230?v=4",
+    followers: 65000,
+    following: 0,
+    location: "Arkansas",
+    scraping_status: "completed"
+  },
+  {
+    name: "Evan You",
+    short_github_url: "https://github.com/yyx990803",
+    github_username: "yyx990803",
+    avatar_url: "https://avatars.githubusercontent.com/u/499550?v=4",
+    followers: 85000,
+    following: 50,
+    location: "Singapore",
+    scraping_status: "completed"
+  },
+  {
+    name: "Dan Abramov",
+    short_github_url: "https://github.com/gaearon",
+    github_username: "gaearon",
+    avatar_url: "https://avatars.githubusercontent.com/u/810438?v=4",
+    followers: 95000,
+    following: 300,
+    location: "London, UK",
+    scraping_status: "completed"
+  },
+  {
+    name: "Tom Preston-Werner",
+    short_github_url: "https://github.com/mojombo",
+    github_username: "mojombo",
+    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+    followers: 35000,
+    following: 150,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
+    name: "Chris Wanstrath",
+    short_github_url: "https://github.com/defunkt",
+    github_username: "defunkt",
+    avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
+    followers: 28000,
+    following: 200,
+    location: "San Francisco",
+    scraping_status: "completed"
+  },
+  {
     name: "Yukihiro Matsumoto",
     short_github_url: "https://github.com/matz",
     github_username: "matz",
-    avatar_url: "https://avatars2.githubusercontent.com/u/30733?s=460&v=4",
-    followers: 7700,
+    avatar_url: "https://avatars.githubusercontent.com/u/30733?v=4",
+    followers: 18000,
     following: 0,
-    location: "Japan"
+    location: "Japan",
+    scraping_status: "completed"
   },
   {
-    name: "DHH",
+    name: "Jeremy Evans",
+    short_github_url: "https://github.com/jeremyevans",
+    github_username: "jeremyevans",
+    avatar_url: "https://avatars.githubusercontent.com/u/16869?v=4",
+    followers: 3000,
+    following: 0,
+    location: "California",
+    scraping_status: "completed"
+  },
+  {
+    name: "Eileen Uchitelle",
+    short_github_url: "https://github.com/eileencodes",
+    github_username: "eileencodes",
+    avatar_url: "https://avatars.githubusercontent.com/u/541234?v=4",
+    followers: 5000,
+    following: 100,
+    location: "Brooklyn, NY",
+    scraping_status: "completed"
+  },
+  {
+    name: "José Valim",
+    short_github_url: "https://github.com/josevalim",
+    github_username: "josevalim",
+    avatar_url: "https://avatars.githubusercontent.com/u/9582?v=4",
+    followers: 30000,
+    following: 200,
+    location: "Poland",
+    scraping_status: "completed"
+  },
+  {
+    name: "Aaron Patterson",
+    short_github_url: "https://github.com/tenderlove",
+    github_username: "tenderlove",
+    avatar_url: "https://avatars.githubusercontent.com/u/3124?v=4",
+    followers: 12000,
+    following: 50,
+    location: "Seattle",
+    scraping_status: "completed"
+  },
+  {
+    name: "Yehuda Katz",
+    short_github_url: "https://github.com/wycats",
+    github_username: "wycats",
+    avatar_url: "https://avatars.githubusercontent.com/u/4?v=4",
+    followers: 25000,
+    following: 100,
+    location: "New York",
+    scraping_status: "completed"
+  },
+  {
+    name: "Rafael França",
+    short_github_url: "https://github.com/rafaelfranca",
+    github_username: "rafaelfranca",
+    avatar_url: "https://avatars.githubusercontent.com/u/47848?v=4",
+    followers: 8000,
+    following: 300,
+    location: "São Paulo, Brazil",
+    scraping_status: "completed"
+  },
+  {
+    name: "Greg Kroah-Hartman",
+    short_github_url: "https://github.com/gregkh",
+    github_username: "gregkh",
+    avatar_url: "https://avatars.githubusercontent.com/u/1128687?v=4",
+    followers: 15000,
+    following: 0,
+    location: "Germany",
+    scraping_status: "completed"
+  },
+  {
+    name: "Adam Wathan",
+    short_github_url: "https://github.com/adamwathan",
+    github_username: "adamwathan",
+    avatar_url: "https://avatars.githubusercontent.com/u/4323180?v=4",
+    followers: 45000,
+    following: 50,
+    location: "Canada",
+    scraping_status: "completed"
+  },
+  {
+    name: "David Heinemeier Hansson",
     short_github_url: "https://github.com/dhh",
     github_username: "dhh",
     avatar_url: "https://avatars.githubusercontent.com/u/2741?v=4",
-    followers: 5000,
+    followers: 90000,
     following: 10,
-    location: "Chicago"
+    location: "Chicago",
+    scraping_status: "completed"
+  },
+  {
+    name: "Linus Torvalds",
+    short_github_url: "https://github.com/torvalds",
+    github_username: "torvalds",
+    avatar_url: "https://avatars.githubusercontent.com/u/1024025?v=4",
+    followers: 200000,
+    following: 0,
+    location: "Portland, OR",
+    scraping_status: "completed"
   }
 ])
 
